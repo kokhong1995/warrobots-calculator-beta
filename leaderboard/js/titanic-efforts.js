a85fb42c45e55203c58aa06b69e81abde0c2b396
kokhong1995 <kokhong.hean@gmail.com>
2026-09-15 14:10:36 +0800

    Fix incorrect URL import and redirect.

--- leaderboard/js/titanic-efforts.js
+++ leaderboard/js/titanic-efforts.js
@@ -1,6 +1,6 @@
-import { toInt, thousandSeperator, getStrAmount } from '/warrobots-calculator/js/data-helper.js?v=1.12.5-beta';
-import { resetInputs, updateNumberInput } from '/warrobots-calculator/js/input-helper.js?v=1.12.5-beta';
-import { applyUpgradeDiscountPercentage, calculateTotalTitanDeployPoints } from '/warrobots-calculator/js/modifier-helper.js?v=1.12.5-beta';
+import { toInt, thousandSeperator, getStrAmount } from '/warrobots-calculator-beta/js/data-helper.js?v=1.12.5-beta';
+import { resetInputs, updateNumberInput } from '/warrobots-calculator-beta/js/input-helper.js?v=1.12.5-beta';
+import { applyUpgradeDiscountPercentage, calculateTotalTitanDeployPoints } from '/warrobots-calculator-beta/js/modifier-helper.js?v=1.12.5-beta';
 
 function updateInputValue(eventType, inputId) {
     updateNumberInput(eventType, inputId, syncData);
